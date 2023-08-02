dd0912aaff4ce1a970c829dacf46dcce85acb140
Felipe Costa <felipecosta776@outlook.com>
2023-08-02 16:03:37 -0300

    updating models

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,19 +1,14 @@
 from rest_framework import serializers
-from watchlist_app.models import Movie
+from watchlist_app.models import WatchList, StreamPlatform
 
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = "__all__"
+class StreamPlatformSerializer(serializers.ModelSerializer):
     
-    def validate(self, data):
-        if data['name'] == data['description']:
-            raise serializers.ValidationError("Title and Description should be different!")
-        else:
-            return data
-        
-    def validate_name(self, value):
-        if len(value) < 2:
-            raise serializers.ValidationError("Name is too short!")
-        else:
-            return value
+    class Meta:
+        model = StreamPlatform
+        fields = "__all__"
+
+class WatchListSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = WatchList
+        fields = "__all__"
