a85182d47a6df46b6c1294a5270e1ca9ed2eb59c
Felipe Costa <felipecosta776@outlook.com>
2023-08-04 18:00:45 -0300

    genericAPIview and mixins

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV
+from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie_list'),
     path('<int:pk>', WatchDetailAV.as_view(), name='movie_detail'),
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
-    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamplatform-detail')
+    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
+    path('review', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail')
 ]
