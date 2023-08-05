10bb31144150b76666551ef26f37b83d1774ec52
Felipe Costa <felipecosta776@outlook.com>
2023-08-05 17:40:25 -0300

    user model

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status, generics, viewsets
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from watchlist_app.models import WatchList, StreamPlatform, Review
@@ -8,11 +9,19 @@ from watchlist_app.api.serializers import (WatchListSerializer, StreamPlatformSe
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
     
+    def get_queryset(self):
+        return Review.objects.all()
+    
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         watchlist = WatchList.objects.get(pk=pk)
         
-        serializer.save(watchlist=watchlist)
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie!")
+        
+        serializer.save(watchlist=watchlist, review_user=review_user)
 
 class ReviewList(generics.ListAPIView):
     serializer_class = ReviewSerializer
