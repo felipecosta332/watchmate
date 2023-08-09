45d7726af48d00c464b41c3f5012e780f6a8b799
Felipe Costa <felipecosta776@outlook.com>
2023-08-08 22:09:22 -0300

    token authentication part 5 registration

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,12 +1,26 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 from user_app.api.serializers import RegistrationSerializer
+from user_app import models
 
 @api_view(['POST',])
 def registration_view(request):
     
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+        
+        data = {}
+        
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            account = serializer.save()
+            
+            data['response'] = "Registration Successful!"
+            data['username'] = account.username
+            data['email'] = account.email
+            data['token'] = Token.objects.get(user=account).key
+            
+        else:
+            data = serializer.errors
+            
+        return Response(data)
