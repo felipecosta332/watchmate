ebcecbbce24b2aee134930126e1d474e0476ceee
Felipe Costa <felipecosta776@outlook.com>
2023-08-10 17:42:57 -0300

    jwt authentication registration

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -2,8 +2,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 from user_app.api.serializers import RegistrationSerializer
-from user_app import models
+# from user_app import models
 
 @api_view(['POST',])
 def logout_view(request):
@@ -26,7 +27,13 @@ def registration_view(request):
             data['response'] = "Registration Successful!"
             data['username'] = account.username
             data['email'] = account.email
-            data['token'] = Token.objects.get(user=account).key
+            # data['token'] = Token.objects.get(user=account).key
+            
+            refresh = RefreshToken.for_user(account)
+            data['token'] = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }
             
         else:
             data = serializer.errors
