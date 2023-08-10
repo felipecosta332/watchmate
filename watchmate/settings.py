815ef2952b2d117709f2be03d80a7ebf4706b626
Felipe Costa <felipecosta776@outlook.com>
2023-08-09 22:36:32 -0300

    jwt auth access and refresh tokens

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -131,6 +131,7 @@ REST_FRAMEWORK = {
         # ],
         'DEFAULT_AUTHENTICATION_CLASSES': [
             # 'rest_framework.authentication.BasicAuthentication',
-            'rest_framework.authentication.TokenAuthentication',
+            # 'rest_framework.authentication.TokenAuthentication',
+            'rest_framework_simplejwt.authentication.JWTAuthentication',
         ]
 }
