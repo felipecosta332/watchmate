c388da886e646247ca63a7f38560ca4ae9e79993
Felipe Costa <felipecosta776@outlook.com>
2023-08-13 17:41:41 -0300

    pagination part 1 pagenumber

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -10,6 +10,7 @@ from watchlist_app.models import WatchList, StreamPlatform, Review
 from watchlist_app.api.permissions import IsAdminOrReadOnly, IsReviewUserOrReadOnly
 from watchlist_app.api.serializers import (WatchListSerializer, StreamPlatformSerializer, ReviewSerializer)
 from watchlist_app.api.throttling import ReviewCreateThrottle, ReviewListThrottle
+from watchlist_app.api.pagination import WatchListPagination
 
 class UserReview(generics.ListAPIView):
     serializer_class = ReviewSerializer
@@ -120,6 +121,7 @@ class StreamPlatformDetailAV(APIView):
 class WatchListGV(generics.ListAPIView):
     queryset = WatchList.objects.all()
     serializer_class = WatchListSerializer
+    pagination_class = WatchListPagination
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['avg_rating']
 
