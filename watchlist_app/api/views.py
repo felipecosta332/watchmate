b7858f61c2c9b7e158e7a7fc354e7a976a7b13d9
Felipe Costa <felipecosta776@outlook.com>
2023-08-15 15:57:07 -0300

    pagination part 2 cursor

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -10,7 +10,7 @@ from watchlist_app.models import WatchList, StreamPlatform, Review
 from watchlist_app.api.permissions import IsAdminOrReadOnly, IsReviewUserOrReadOnly
 from watchlist_app.api.serializers import (WatchListSerializer, StreamPlatformSerializer, ReviewSerializer)
 from watchlist_app.api.throttling import ReviewCreateThrottle, ReviewListThrottle
-from watchlist_app.api.pagination import WatchListPagination, WatchListLOPagination 
+from watchlist_app.api.pagination import WatchListPagination, WatchListLOPagination, WatchListCPagination 
 
 class UserReview(generics.ListAPIView):
     serializer_class = ReviewSerializer
@@ -121,9 +121,9 @@ class StreamPlatformDetailAV(APIView):
 class WatchListGV(generics.ListAPIView):
     queryset = WatchList.objects.all()
     serializer_class = WatchListSerializer
-    pagination_class = WatchListLOPagination
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['avg_rating']
+    pagination_class = WatchListCPagination
+    # filter_backends = [filters.OrderingFilter]
+    # ordering_fields = ['avg_rating']
 
 class WatchListAV(APIView):
     
